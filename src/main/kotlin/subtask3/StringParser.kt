45d672ab468f2cc554448f619df4ad6027902e03
MDPetrovich <greshnik578@yandex.ru>
2021-05-31 19:21:02 +0300

    fix:task3

--- src/main/kotlin/subtask3/StringParser.kt
+++ src/main/kotlin/subtask3/StringParser.kt
@@ -4,52 +4,46 @@ class StringParser {
 
     // TODO: Complete the following function
     fun getResult(inputString: String): Array<String> {
-        val firstEl1 = "("
-        val secondEl1 = ")"
-        val firstEl2 = "["
-        val secondEl2 = "]"
-        val firstEl3 = "«"
-        val secondEl3 = "»"
-
-        var resArr = mutableListOf<String>()
-
-        if(inputString.contains(firstEl1)&&inputString.contains(secondEl1)&&inputString.indexOf(firstEl1)<inputString.lastIndexOf(secondEl1))
-        {
-            var substrFirst = inputString.substring(inputString.indexOf(firstEl1), inputString.lastIndexOf(secondEl1)+1)
-            println(substrFirst)
-            while(substrFirst.contains(firstEl1)&&substrFirst.contains(secondEl1)&&substrFirst.indexOf(firstEl1)<substrFirst.lastIndexOf(secondEl1)){
-                resArr.add(substrFirst.substring(substrFirst.indexOf(firstEl1)+1, substrFirst.indexOf(secondEl1)))
-                if(substrFirst.indexOf(secondEl1)==substrFirst.lastIndexOf(secondEl1))
-                    break
-                substrFirst = substrFirst.substring(substrFirst.indexOf(secondEl1)+1, substrFirst.lastIndexOf(secondEl1))
+        var s = emptyArray<String>()
+        for (i in inputString.indices) {
+            if (inputString[i] =='('){
+                var st = ""
+                var h = i+1
+                for (j in inputString.indices){
+                    if (inputString[h] !=')') {
+                        st += inputString[h]
+                        println(st)
+                        h += 1
+                    }
+                }
+                s += st
+            }
+            if (inputString[i] =='<'){
+                var st = ""
+                var h = i+1
+                for (j in inputString.indices){
+                    if (inputString[h] !='>') {
+                        st += inputString[h]
+                        println(st)
+                        h += 1
+                    }
+                }
+                s += st
+            }
+            if (inputString[i] =='['){
+                var st = ""
+                var h = i+1
+                for (j in inputString.indices){
+                    if (inputString[h] !=']') {
+                        st += inputString[h]
+                        println(st)
+                        h += 1
+                    }
+                }
+                s += st
             }
         }
-        if(inputString.contains(firstEl2)&&inputString.contains(secondEl2)&&inputString.indexOf(firstEl2)<inputString.lastIndexOf(secondEl2))
-        {
-            var substrFirst = inputString.substring(inputString.indexOf(firstEl2), inputString.lastIndexOf(secondEl2)+1)
-            println(substrFirst)
-            while(substrFirst.contains(firstEl2)&&substrFirst.contains(secondEl2)&&substrFirst.indexOf(firstEl2)<substrFirst.lastIndexOf(secondEl2)){
-                resArr.add(substrFirst.substring(substrFirst.indexOf(firstEl2)+1, substrFirst.indexOf(secondEl2)))
-                if(substrFirst.indexOf(secondEl2)==substrFirst.lastIndexOf(secondEl2))
-                    break
-                substrFirst = substrFirst.substring(substrFirst.indexOf(secondEl2)+1, substrFirst.lastIndexOf(secondEl2))
-            }
-        }
-
-        if(inputString.contains(firstEl3)&&inputString.contains(secondEl3)&&inputString.indexOf(firstEl3)<inputString.lastIndexOf(secondEl3))
-        {
-            var substrFirst = inputString.substring(inputString.indexOf(firstEl3), inputString.lastIndexOf(secondEl3)+1)
-            while(substrFirst.contains(firstEl3)&&substrFirst.contains(secondEl3)&&substrFirst.indexOf(firstEl3)<substrFirst.lastIndexOf(secondEl3)){
-                resArr.add(substrFirst.substring(substrFirst.indexOf(firstEl3)+1, substrFirst.indexOf(secondEl3)))
-                if(substrFirst.indexOf(secondEl3)==substrFirst.lastIndexOf(secondEl3))
-                    break
-                substrFirst = substrFirst.substring(substrFirst.indexOf(secondEl3)+1, substrFirst.lastIndexOf(secondEl3))
-            }
-        }
-
-
-        return resArr.toTypedArray()
-        throw NotImplementedError("Not implemented")
+        return s
     }
 }
 /*3
